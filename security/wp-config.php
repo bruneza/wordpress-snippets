3371c430fe2f2b55f3e3a055cc4a4b92fc3a8110
Bruce Mugwaneza <82029322+bruneza@users.noreply.github.com>
2023-02-03 12:19:05 +0200

    change website login url

--- security/wp-config.php
+++ security/wp-config.php
@@ -4,3 +4,6 @@
 
 //Disable script file concatenation
 define('CONCATENATE_SCRIPTS', false);
+
+define('WP_ADMIN_DIR', 'secret-folder');
+define( 'ADMIN_COOKIE_PATH', SITECOOKIEPATH . WP_ADMIN_DIR);
